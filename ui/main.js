0bbf272c407af2eff8e790394ffdebe68c3b100a
Shahnaj shaik <30707246+skshahnaj@users.noreply.github.com>
2017-08-07 05:56:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,13 @@ element.innerHTML='New Value';
 //move the image
 var img=document.getElementById('madi');
 
+var marginLeft=0;
+function moveRight(){
+  marginLeft=marginLeft+10;
+  img.style.marginLeft=marginLeft + 'px';
+}
+
 img.onclick=function(){
-    img.style.marginLeft='100px';
+    
+    var interval=setInterval(moveRight, 100);
 };
