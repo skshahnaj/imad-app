0c73361a4052190959745dce75b1f91477d15b72
Shahnaj shaik <30707246+skshahnaj@users.noreply.github.com>
2017-08-08 17:18:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,7 +26,7 @@ button.onclick=function(){
 
 };
 
-
+// submit names
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
 // make a request to the server and send the name
@@ -61,6 +61,45 @@ submit.onclick=function(){
     
   // Make the request.
   request.open('GET','http://nshannughf.imad.hasura-app.io/submit-name?name=' + name,true);
+  request.send(null);
+    
+};
+
+// post a comment
+var post=document.getElementById('post_btn');
+post.onclick=function(){
+// make a request to the server and send the name
+
+// create a request object
+  var request=new XMLHttpRequest();
+  
+  //Capture the response and store it in a variable.
+  request.onreadystatechange = function(){
+    if(request.readyState === XMLHttpRequest.DONE)  
+    {
+        if(request.status === 200)
+        {
+            // capture a list of names and render it as a list
+                var comments= request.responseText;
+                comments=JSON.parse(comments);
+                var list="";
+                for(var i=0; i<comments.length;i++)
+                {
+                    list +='<li>'+comments[i]+'</li>';
+                    
+                }
+                var ul=document.getElementById('commentslist');
+                ul.innerHTML=list;
+        }
+    }
+  };
+  
+  // submit name
+    var commentInput=document.getElementById('comments');
+    var comment=commentInput.value;
+    
+  // Make the request.
+  request.open('GET','http://nshannughf.imad.hasura-app.io/:articleName/post-comment?comment=' + comment,true);
   request.send(null);
     
 };
