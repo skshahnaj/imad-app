7dab021ffe3b7dfa0ab4cf96b4bc6cdeffd57fef
Shahnaj shaik <30707246+skshahnaj@users.noreply.github.com>
2017-08-14 13:57:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -148,7 +148,7 @@ app.get('/articles/:articleName', function(req,res){
             res.status(500).send(err.toString());
         }else{
             if(result.rows.length === 0){
-                red.status(404).send('Article not found');
+                res.status(404).send('Article not found');
             } else {
                 var articleData=result.rows[0];
                 res.send(createTemplate(articleData));
