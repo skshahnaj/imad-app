5a7130b92c936253425b884b4ec94a8ec9a9e11c
Shahnaj shaik <30707246+skshahnaj@users.noreply.github.com>
2017-08-14 13:11:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,11 +136,24 @@ app.get('/submit-name', function(req,res){ // URL: /submit-name?name=XXXXXX
 });
 
 
-app.get('/:articleName', function(req,res){
+app.get('/articles/:articleName', function(req,res){
     //articleName==article-article-one
     //articles[articleName]=={} content object for article-one
     var articleName=req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+    
+    pool.query("select * from article where title="+ req.params.articleName,function(err,result){
+        if(err){
+            res.status(500).send(err.tostring());
+        }else{
+            if(result.rows.length===0){
+                red.status(404).send('Article not found');
+            } else {
+                var articleData=result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
+    
 });
 
 var comments=[];
