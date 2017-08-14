b716e428ccb1df4e22587bd7a707436ae31e5abe
Shahnaj shaik <30707246+skshahnaj@users.noreply.github.com>
2017-08-14 12:47:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var path = require('path');
 var Pool=require('pg').Pool;
 
 var config={
-  user:'nshannughf',
-  database:'nshannughf',
-  host:'db.imad.hasura-app.io',
-  port:'5432',
-  password:process.env.DB_PASSWORD
+  user: 'nshannughf',
+  database: 'nshannughf',
+  host: 'db.imad.hasura-app.io',
+  port: '5432',
+  password: process.env.DB_PASSWORD
 };
 
 
@@ -105,9 +105,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool=new Pool(config);
 app.get('/test-db', function(req,res){
    // make a select request
    // return the response with the results
+   pool.query('select * from test', function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       }else{
+            res.send(JSON.stringify(result));
+       }
+   });
    
 });
 
