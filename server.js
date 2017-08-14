6268587c6202dc4b6d146337c52870c43ce4d450
Shahnaj shaik <30707246+skshahnaj@users.noreply.github.com>
2017-08-14 13:24:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -141,9 +141,9 @@ app.get('/articles/:articleName', function(req,res){
     //articles[articleName]=={} content object for article-one
     var articleName=req.params.articleName;
     
-    pool.query("select * from article where title="+ req.params.articleName,function(err,result){
+    pool.query("select * from article where title='"+ req.params.articleName + "'",function(err,result){
         if(err){
-            res.status(500).send(err.tostring());
+            res.status(500).send(err.toString());
         }else{
             if(result.rows.length===0){
                 red.status(404).send('Article not found');
