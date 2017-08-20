0e651a0b704900e88fd0e84a68f5e8ffb1907451
Shahnaj shaik <30707246+skshahnaj@users.noreply.github.com>
2017-08-20 06:49:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,31 +1,3 @@
-// Counter code
-var button=document.getElementById('counter');
-
-
-button.onclick=function(){
-  // create a request object
-  var request=new XMLHttpRequest();
-  
-  //Capture the response and store it in a variable.
-  request.onreadystatechange = function(){
-    if(request.readyState === XMLHttpRequest.DONE)  
-    {
-        if(request.status === 200)
-        {
-            var counter= request.responseText;
-            var span=document.getElementById('count');
-            span.innerHTML=counter.toString();
-        }
-    }
-  };
-  
-  // Make the request.
-  
-  request.open('GET','http://nshannughf.imad.hasura-app.io/counter',true);
-  request.send(null);
-
-};
-
 
 // submit username/password to login
 var submit=document.getElementById('submit_btn');
@@ -66,41 +38,3 @@ submit.onclick=function(){
 
 
 
-// post a comment
-var post=document.getElementById('post_btn');
-post.onclick=function(){
-// make a request to the server and send the name
-
-// create a request object
-  var request=new XMLHttpRequest();
-  
-  //Capture the response and store it in a variable.
-  request.onreadystatechange = function(){
-    if(request.readyState === XMLHttpRequest.DONE)  
-    {
-        if(request.status === 200)
-        {
-            // capture a list of names and render it as a list
-                var comments= request.responseText;
-                comments=JSON.parse(comments);
-                var list="";
-                for(var i=0; i<comments.length;i++)
-                {
-                    list +='<li>'+comments[i]+'</li>';
-                    
-                }
-                var ul=document.getElementById('commentslist');
-                ul.innerHTML=list;
-        }
-    }
-  };
-  
-  // submit name
-    var commentInput=document.getElementById('comments');
-    var comment=commentInput.value;
-    
-  // Make the request.
-  request.open('GET','http://nshannughf.imad.hasura-app.io/:articleName/post-comment?comment=' + comment,true);
-  request.send(null);
-    
-};
